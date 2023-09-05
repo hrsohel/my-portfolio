d40d312f2f8956777ebf5768c7913878eff59a66
Sohel <hrsohel679@gmail.com>
2023-09-05 10:28:45 +0600

    change in home page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,11 @@
 "use client";
 
 import HomeComponent from "@/components/HomeComponent";
-import Loader from "@/components/Loader";
 import Navbar from "@/components/Navbar";
 import React from "react";
-import { useRouter } from "next/router";
 import Footer from "@/components/Footer";
 
 const page = () => {
-  const res = fetch("/api").then(() => console.log("data"));
   return (
     <>
       <Navbar />
