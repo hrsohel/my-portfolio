32747a341701f2d01c45ffc150012d8312749883
Sohel <hrsohel679@gmail.com>
2023-09-08 17:11:50 +0600

    updated full of uploadImage file

--- controllers/uploadImage.js
+++ controllers/uploadImage.js
@@ -1,14 +1,19 @@
 import { writeFile } from "fs/promises";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 export async function uploadImage(image) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
   let fileName;
   if (image) {
     const name = image.name.split(".")[0];
     const ext = image.name.split(".")[1];
     fileName = `${name}${Math.random()}.${ext}`;
     const bytes = await image.arrayBuffer();
+    const path = join(__dirname, "..");
     const buffer = Buffer.from(bytes);
-    await writeFile(`./public/uploads/${fileName}`, buffer);
+    await writeFile(`${path}/public/uploads/${fileName}`, buffer);
   }
   return fileName;
 }
